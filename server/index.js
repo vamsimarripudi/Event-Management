84642fdf60fd1ee6d161865043735b0c2bc82e08
vamsi marripudi <vmarripudi49@gmail.com>
2026-03-12 14:15:14 +0530

    root changes

--- server/index.js
+++ server/index.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({extended:true}));
+app.get("/", (req,res) => {
+    res.send("Welcome to the Event Management API");
+}); 
 app.use("/api/auth", authRoutes);
 app.use("/api/event", eventRoutes);
 
