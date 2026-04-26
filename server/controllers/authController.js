136188deffb0a601829c5f4f79efe6d36767248e
vamsi marripudi <vmarripudi49@gmail.com>
2026-04-26 12:17:26 +0530

    Added new route called get user by id.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const mongoose = require('mongoose')
 
 const register = async(req,res)=> {
     try{
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const authController = require("../controllers/authController");
 const {register,login,getUserById} = authController;
-const verifyToken = require("../middleware")
+
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/users/:id",verifyToken,getUserById);
+router.get("/users/:id",getUserById);
 
 module.exports = router;
