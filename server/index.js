cf96079918e68fbfc55095d0d3f951e36318af1a
vamsi marripudi <vmarripudi49@gmail.com>
2026-04-29 21:08:10 +0530

    Database error is resolved and now everything is normal.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const connectDB = require("./db");
 const authRoutes = require("./routes/authRoute");
@@ -8,8 +9,6 @@ const feedbackRoute = require("./routes/feedbackRoute");
 const verifyToken = require("./middleware/token");
 
 
-
-require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
