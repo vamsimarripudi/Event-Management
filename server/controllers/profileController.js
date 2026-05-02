9599227bce96a858f2665bd610e7d3499dadb789
vamsi marripudi <vmarripudi49@gmail.com>
2026-05-02 12:39:05 +0530

    Pushing.

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -3,6 +3,7 @@ const User = require("../models/User");
 const getUserProfile = async(req,res)=>{
     try{
         console.log("User details", req,user)
+        const userId = req.user._id || req.user.id
         const user = await User.findById(req.user._id).select("-password");
         if(!user){
             res.status(404).json({message: "User Not Found"})
