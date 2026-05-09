9960ef7604a6a1ccce7926a1ac1f69db8b5745ff
vamsi marripudi <vmarripudi49@gmail.com>
2026-05-09 19:12:08 +0530

    .

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middleware/token");
 const { sendNewsletter } = require("../controllers/adminController");
-const verifyToken = require("../middleware/token");
+
 
 router.post("/newsletter", verifyToken, sendNewsletter);
 
